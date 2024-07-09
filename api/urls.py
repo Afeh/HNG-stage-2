81f234175f91defbde55a6d58c04b9adf1154f4d
Afebu Balogun <balogunafebu22@gmail.com>
2024-07-09 11:46:03 +0100

    removed slahses

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-	path('auth/register/', RegisterView.as_view(), name='register'),
-	path('auth/login/', LoginView.as_view(), name='login'),
-	path('api/users/<str:user_id>/', UserDetailView.as_view(), name='user_details'),
-	path('api/organisations/', OrganisationView.as_view(), name='organisation'),
-	path('api/organisations/<str:org_id>/', OrganisationDetailView.as_view(), name='organisation-detail'),
+	path('auth/register', RegisterView.as_view(), name='register'),
+	path('auth/login', LoginView.as_view(), name='login'),
+	path('api/users/<str:user_id>', UserDetailView.as_view(), name='user_details'),
+	path('api/organisation', OrganisationView.as_view(), name='organisation'),
+	path('api/organisations/<str:org_id>', OrganisationDetailView.as_view(), name='organisation-detail'),
 	path('api/organisations/<str:org_id>/users', OrganisationUserView.as_view(), name='organisation-users'),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh')
